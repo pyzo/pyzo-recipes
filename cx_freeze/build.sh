63e2502155b1611e2b181b8f28cfc182e603bc02
almar <almar@localhost>
2013-10-16 14:16:36 +0200

    fix cx_freeze recipe for osx. Crap workaround, but at least it works.

--- cx_freeze/build.sh
+++ cx_freeze/build.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-$PYTHON setup.py install
+if [ `uname` == Linux ]; then
+    $PYTHON setup.py install
+fi
 
-# Add more build steps here, if they are necessary.
 
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
+if [ `uname` == Darwin ]; then
+
+    # This does not work:
+    #sudo python3.3 setup.py install
+    
+    # So install cx_freeze in whatever way in native python
+    
+    # Test
+    python3.3 -c 'import cx_Freeze'
+    
+    # Install from there
+    $PYTHON $RECIPE_DIR/../pyside-pyzo/build_from_installed.py /opt/local/Library/Frameworks/Python.framework/Versions/3.3/lib/python3.3/site-packages/cx_Freeze
+    
+fi
