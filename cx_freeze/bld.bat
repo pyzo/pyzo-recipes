fb95b9782681e5a4b90b31da7aabb1f97e4b7463
almar <almar@localhost>
2013-10-18 15:05:00 +0200

    Make cx_freeze recipe work for both 32 and 64 bit win

--- cx_freeze/bld.bat
+++ cx_freeze/bld.bat
@@ -1,9 +1,11 @@
-"c:\Program Files\7-Zip\7z.exe" x -otmp -aos cx_Freeze-4.3.1.win32-py3.3.exe
+
+if defined ProgramFiles(x86) (
+    "c:\Program Files\7-Zip\7z.exe" x -otmp -aos cx_Freeze-4.3.1.win-amd64-py3.3.exe
+
+) else (
+    "c:\Program Files\7-Zip\7z.exe" x -otmp -aos cx_Freeze-4.3.1.win32-py3.3.exe
+
+)
 
 xcopy tmp\PLATLIB %SP_DIR% /E /I
 xcopy tmp\SCRIPTS %SCRIPTS% /E /I
-
-REM if "%PY3K%"=="1" (
-REM     %SYS_PREFIX%\Scripts\prepend-dlls %SP_DIR%\cx_Freeze\__init__.py
-REM     if errorlevel 1 exit 1
-REM )
