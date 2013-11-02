40c46455392c7b8bdf0520df6219b2399b316d6c
almar <almar@localhost>
2013-11-03 00:21:38 +0100

    fix sklearn recipe for linux

--- scikit-learn/build.sh
+++ scikit-learn/build.sh
@@ -1,21 +1,2 @@
 #!/bin/bash
-
-if [ `uname` == Linux ]; then
-    $PYTHON setup.py install
-fi
-
-
-if [ `uname` == Darwin ]; then
-
-    # This does not work:
-    #sudo python3.3 setup.py install
-    
-    # So install cx_freeze in whatever way in native python
-    
-    # Test
-    python3.3 -c 'import cx_Freeze'
-    
-    # Install from there
-    $PYTHON $RECIPE_DIR/../pyside-pyzo/build_from_installed.py /opt/local/Library/Frameworks/Python.framework/Versions/3.3/lib/python3.3/site-packages/cx_Freeze
-    
-fi
+$PYTHON setup.py install
