2ef9290d43f81d7f280eddef61576426a0327365
almar <almar@localhost>
2014-04-10 22:46:18 +0200

    New cx_freeze (py34) and PySide on 3.4!

--- pyside-pyzo/bld.bat
+++ pyside-pyzo/bld.bat
@@ -1,8 +1,8 @@
 if defined ProgramFiles(x86) (
-    "c:\Program Files\7-Zip\7z.exe" x -otmp -aos PySide-1.2.1.win-amd64-py3.3.exe
+    "c:\Program Files\7-Zip\7z.exe" x -otmp -aos PySide-1.2.1.win-amd64-py3.4.exe
 
 ) else (
-    "c:\Program Files\7-Zip\7z.exe" x -otmp -aos PySide-1.2.1.win32-py3.3.exe
+    "c:\Program Files\7-Zip\7z.exe" x -otmp -aos PySide-1.2.1.win32-py3.4.exe
 
 )
 
--- pyside-pyzo/build.sh
+++ pyside-pyzo/build.sh
@@ -58,7 +58,7 @@ set -e
 $PYTHON -c '
 import sys, os, sysconfig
 incl_dir = sysconfig.get_path("include")
-lib_dir = sys.prefix + "/lib/libpython3.3m.so"
+lib_dir = sys.prefix + "/lib/libpython3.4m.so"
 if not os.path.isfile(lib_dir):
   lib_dir = lib_dir.replace(".so",".dylib")
 print("Patching build/CMakeCache.txt ...")
