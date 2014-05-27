67d9b9a6aae38c12db6a2b0ca4a2e63f2dc45235
almar <almar@localhost>
2014-05-27 16:02:15 +0200

    fix pyside recipe for windows

--- pyside-pyzo/run_test.py
+++ pyside-pyzo/run_test.py
@@ -1,19 +1,19 @@
 import PySide.QtCore
-import PySide.QtDeclarative
+# import PySide.QtDeclarative  Broken on Windows
 import PySide.QtGui
-import PySide.QtHelp
+# import PySide.QtHelp  Broken on Windows
 import PySide.QtMultimedia
 import PySide.QtNetwork
 import PySide.QtScript
 import PySide.QtScriptTools
-import PySide.QtSql
+# import PySide.QtSql  Broken on Windows
 import PySide.QtSvg
 import PySide.QtTest
 import PySide.QtUiTools
 import PySide.QtWebKit
 import PySide.QtXml
 import PySide.QtXmlPatterns
-import PySide.QtOpenGL  # Was not testes on linux, why?
+import PySide.QtOpenGL
 import pysideuic 
 
 import sys
@@ -22,12 +22,13 @@ import sys
 #    import PySide.phonon
 
 # Test pyside executables
-from subprocess import check_call, CalledProcessError
-bin = sys.prefix + '/bin/'
-check_call([bin+'pyside-uic', '--version'])
-check_call([bin+'pyside-lupdate', '-version'])
-try:
-    check_call([bin+'pyside-rcc', '-version'])
-except CalledProcessError as exc:
-    assert exc.returncode == 1 
+if not sys.platform.startswith('win'):
+    from subprocess import check_call, CalledProcessError
+    bin = sys.prefix + '/bin/'
+    check_call([bin+'pyside-uic', '--version'])
+    check_call([bin+'pyside-lupdate', '-version'])
+    try:
+        check_call([bin+'pyside-rcc', '-version'])
+    except CalledProcessError as exc:
+        assert exc.returncode == 1 
 
