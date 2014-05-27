1d8e725ca22db87b4d1ee259621d4e15b8af6734
almar <almar@localhost>
2014-05-27 16:38:30 +0200

    update recipe for scikit-learn

--- scikit-learn/bld.bat
+++ scikit-learn/bld.bat
@@ -1,9 +1,9 @@
 
 if defined ProgramFiles(x86) (
-    "c:\Program Files\7-Zip\7z.exe" x -otmp -aos scikit-learn-0.14.1.win-amd64-py3.3.exe
+    "c:\Program Files\7-Zip\7z.exe" x -otmp -aos scikit-learn-0.15-git.win-amd64-py3.4.exe
 
 ) else (
-    "c:\Program Files\7-Zip\7z.exe" x -otmp -aos scikit-learn-0.14.1.win32-py3.3.exe
+    "c:\Program Files\7-Zip\7z.exe" x -otmp -aos scikit-learn-0.15-git.win32-py3.4.exe
 
 )
 
