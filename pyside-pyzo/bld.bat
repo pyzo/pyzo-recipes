cf8d4f3e4df2ed0d78ea3c012e7a9ef709cd74be
almar <almar@localhost>
2014-05-27 16:02:15 +0200

    fix pyside recipe for windows

--- pyside-pyzo/bld.bat
+++ pyside-pyzo/bld.bat
@@ -1,12 +1,12 @@
 if defined ProgramFiles(x86) (
-    "c:\Program Files\7-Zip\7z.exe" x -otmp -aos PySide-1.2.1.win-amd64-py3.4.exe
+    "c:\Program Files\7-Zip\7z.exe" x -otmp -aos PySide-1.2.2.win-amd64-py3.4.exe
 
 ) else (
-    "c:\Program Files\7-Zip\7z.exe" x -otmp -aos PySide-1.2.1.win32-py3.4.exe
+    "c:\Program Files\7-Zip\7z.exe" x -otmp -aos PySide-1.2.2.win32-py3.4.exe
 
 )
 
-xcopy tmp\PURELIB %SP_DIR% /E /I
+xcopy tmp\PLATLIB %SP_DIR% /E /I
 xcopy tmp\SCRIPTS %SCRIPTS% /E /I
 
 if errorlevel 1 exit 1
